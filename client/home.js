00dec0e13da2ff14760fab229569f5fa35811e57
Mark Nguyen <pnguyen1097@gmail.com>
2014-11-15 22:08:10 -0500

    Add game manager.

--- client/home.js
+++ client/home.js
@@ -1,17 +1,7 @@
 Template.home.rendered = function() {
 
-  //TODO: move this else where.
+  var manager = new Spacehole.GameManager();
 
-  var level = new Spacehole.Level();
-
-  var renderer = new Spacehole.LevelRenderer(level, "viewport", 800, 600);
-
-  var world = level.world;
-
-  Physics.util.ticker.on(function( time, dt ){
-    world.step(time);
-  });
-
-  Physics.util.ticker.start();
+  manager.restart();
 
 }

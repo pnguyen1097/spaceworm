a8140978c14dbaef3c9d80618dd9b9bbda7a1dda
Alex Vargas <vargas.84@osu.edu>
2014-11-16 01:45:24 -0500

    Add Sprites

--- packages/spacehole/spacehole/DynamicStar.js
+++ packages/spacehole/spacehole/DynamicStar.js
@@ -2,7 +2,7 @@ var Physics = this.Physics;
 
 function DynamicStar(options) {
   var body = new Physics.body('circle', options);
-  body.body_type = 'dynamic_star';
+  body.body_type = 'star';
 
   return body;
 }
--- packages/spacehole/spacehole/StaticStar.js
+++ packages/spacehole/spacehole/StaticStar.js
@@ -4,7 +4,7 @@ function StaticStar(options) {
   options.treatment = 'static';
 
   var body = new Physics.body('circle', options);
-  body.body_type = 'static_star';
+  body.body_type = 'star';
 
   return body;
 }
